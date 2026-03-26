09a4c49ef6af692996ae93a763b702e98c50bd72
Link4Electronics <link4electronics@gmail.com>
2026-03-26 11:34:14 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,9 @@ else
     #make-aur-package mixxx-git
     git clone https://gitlab.archlinux.org/archlinux/packaging/packages/mixxx
     cd mixxx
+    useradd -m builder
+    echo "builder ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/builder
+    sudo chown -R builder:builder .
     makepkg -si
 fi
 
